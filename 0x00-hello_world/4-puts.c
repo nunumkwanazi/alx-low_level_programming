2067c2a9177e50811229f9ad919b54904302b57a
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-06-06 13:10:22 -0700

    My 5th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,3 +11,15 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+#include <stdio.h>
+
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

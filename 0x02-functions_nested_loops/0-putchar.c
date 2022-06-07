1023e6d02ea8222f0d6c1100588d63727d390c2a
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-06-07 12:08:19 -0700

    My 1st commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+README.md _putchar(H);
+README.md _putchar(o);
+README.md _putchar(l);
+README.md _putchar(b);
+README.md _putchar(e);
+README.md _putchar(r);
+README.md _putchar(t);
+README.md _putchar(o);
+README.md _putchar(n);
+README.md _putchar(n);
+
+README.md return (0);
+}

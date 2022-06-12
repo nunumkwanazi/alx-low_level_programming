b854e6aab7ec8865b204e0f94b09694a07f78ec2
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-06-12 06:03:53 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar (ch);
+	putchar ('\n');
+	return (0);
+}

56e74af508432a6a84bd0778cbd6bc0d9e0eaf2d
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-06-14 05:53:40 -0700

    Task 0, commit 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,20 @@
 #include "main.h"
-
 /**
- * _isupper - checks if a character is uppercase
- *@c: input char
- *
- * Return : 1 if uppercase and 0 if not
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }

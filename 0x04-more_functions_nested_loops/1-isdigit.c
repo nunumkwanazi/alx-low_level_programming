5d80832848215cac25d9189331e2876e49a6de7a
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-06-14 05:56:16 -0700

    Task 1, commit 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * _isdigit - checks for a digit from 0 to 9
- * @c : int type
- *
- * Return: 1 for digit 0 for anything else
+ * _isdigit - if is a digit
+ *@c: print int
+ * Return: Always 0.
  */
+
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	else 
-	{
-		return (0);
-	}
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
 }

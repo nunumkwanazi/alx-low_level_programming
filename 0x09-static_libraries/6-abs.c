015fb4297c8282700ed07eb22467f599cf947831
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-06-27 06:41:51 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function that prints the absolute value of a number
+ * @n: integer
+ * Return: AlwAYS 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}

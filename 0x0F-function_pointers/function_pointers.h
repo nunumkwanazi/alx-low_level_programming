cc7eef7af4ba6fff039dc77e0f03576c08677ed3
Nunu Mkwanazi <nunu.mkwanazi1@gmail.com>
2022-07-07 11:03:53 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,15 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+/*
+ * File: function_pointers.h
+ * Author: Idi Wakasso
+ */
 
+#include <stdio.h>
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-
-#endi
+#endif /*FUNC_POINTERS*/
